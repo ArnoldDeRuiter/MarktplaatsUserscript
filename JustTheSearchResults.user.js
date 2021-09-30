2f2570cf927b7154f571cc298e1da777e60e9d64
Arnold de Ruiter <187004+ArnoldDeRuiter@users.noreply.github.com>
2021-09-30 16:31:44 +0200

    Clean up javascript code using standardjs

--- JustTheSearchResults.user.js
+++ JustTheSearchResults.user.js
@@ -20,123 +20,122 @@
 // @grant          GM_addStyle
 // ==/UserScript==
 /* globals jQuery, $, waitForKeyElements */
-/*- The @grant directive is needed for GreaseMonkey users.
+/* - The @grant directive is needed for GreaseMonkey users.
     A work around for a design change introduced in GM 1.0. It restores the sandbox.
 */
 
-function alterHome() {
-    // Laat op Home de checkbox "Zoek in titel en beschrijving" zien als optie.
-    $("mp-header.x-scope.mp-header-0.u-stickyHeader").addClass("expandSearchBar mp-Header--expandSearchBar");
+function alterHome () {
+  // Laat op Home de checkbox "Zoek in titel en beschrijving" zien als optie.
+  $('mp-header.x-scope.mp-header-0.u-stickyHeader').addClass('expandSearchBar mp-Header--expandSearchBar')
 
-    // Automatische focus op de zoekblak, zoals eBay dit heeft
-    $('.mp-SearchForm-query #input').focus();
+  // Automatische focus op de zoekblak, zoals eBay dit heeft
+  $('.mp-SearchForm-query #input').focus()
 }
-waitForKeyElements("mp-header.x-scope.mp-header-0.u-stickyHeader", alterHome);
+waitForKeyElements('mp-header.x-scope.mp-header-0.u-stickyHeader', alterHome)
 
-function preventLoginNudge() {
-    localStorage.SeenLoginGate = true;
-    var dt = new Date();
-    date = dt.getDate();
-    var minutes = dt.getMinutes();
-    minutes = minutes > 9 ? minutes : '0' + minutes;
-    var seconds = dt.getSeconds();
-    seconds = seconds > 9 ? seconds : '0' + seconds;
-    var ms = dt.getMilliseconds();
-    ms = ms > 99 ? ms : ms > 9 ? '0' + ms : '00' + ms;
-    dt.setMonth(dt.getMonth() + 1, 1);
-    dt.setHours(dt.getHours() -1, 1);
-    var date = dt.getFullYear() + "-" + dt.getMonth() + "-" + date + "T";
-    var timeOne = dt.getHours() + ":" + minutes + ":" + seconds + "." + ms + "Z";
-    var timeTwo = dt.getHours() + ":" + minutes + ":" + (seconds+11) + "." + ms + "Z";
-    console.log('["'+date+timeOne+'","'+date+timeTwo+'"]');
-    localStorage.MpLoginNudgeDismissDate = '["'+date+timeOne+'","'+date+timeTwo+'"]';
+function preventLoginNudge () {
+  localStorage.SeenLoginGate = true
+  let dt = new Date()
+  date = dt.getDate()
+  let minutes = dt.getMinutes()
+  minutes = minutes > 9 ? minutes : '0' + minutes
+  let seconds = dt.getSeconds()
+  seconds = seconds > 9 ? seconds : '0' + seconds
+  let ms = dt.getMilliseconds()
+  ms = ms > 99 ? ms : ms > 9 ? '0' + ms : '00' + ms
+  dt.setMonth(dt.getMonth() + 1, 1)
+  dt.setHours(dt.getHours() - 1, 1)
+  var date = dt.getFullYear() + '-' + dt.getMonth() + '-' + date + 'T'
+  let timeOne = dt.getHours() + ':' + minutes + ':' + seconds + '.' + ms + 'Z'
+  let timeTwo = dt.getHours() + ':' + minutes + ':' + (seconds + 11) + '.' + ms + 'Z'
+  console.log('["' + date + timeOne + '","' + date + timeTwo + '"]')
+  localStorage.MpLoginNudgeDismissDate = '["' + date + timeOne + '","' + date + timeTwo + '"]'
 }
 // Unless MpLoginSuccess cookie is set (Logged in)
 if (!document.cookie.match(/^(.*;)?\s*MpLoginSuccess\s*=\s*[^;]+(.*)?$/)) {
-    if (localStorage.getItem("SeenLoginGate") === null) {
-        preventLoginNudge();
+  if (localStorage.getItem('SeenLoginGate') === null) {
+    preventLoginNudge()
+  }
+}
+
+function alterElsewhere () {
+  // Laat elders de checkbox "Zoek in titel en beschrijving" zien als optie.
+  $('header.u-stickyHeader').attr('data-expanded', 'true')
+  $('div.mp-Header.mp-text-paragraph.mp-cloak').addClass('mp-Header--expandSearchBar')
+}
+waitForKeyElements('header.u-stickyHeader div.mp-Header.mp-text-paragraph.mp-cloak', alterElsewhere)
+
+function alterSearchResults () {
+  // Verberg betaalde advertenties en bedrijven
+  $('.mp-Listing--list-item').filter(':contains(Bezorgt in)').hide()
+  $('.mp-Listing--list-item').filter(':contains(Topadvertentie)').hide()
+  $('.mp-Listing--list-item').filter(':contains(Dagtopper)').hide()
+  $('.mp-Listing--list-item').filter(':contains(Heel Nederland)').hide()
+  $('.mp-Listing--list-item').filter(':contains(Bezoek website)').hide()
+
+  // Nutteloze "Deze adverteerder heeft meer advertenties" elementen verbergen
+  $('.mp-Listing--other-seller').hide()
+
+  // Adverteerders blokkeren
+  $('.mp-Listing--list-item').filter(':contains(Example Name 123)').hide()
+
+  // Geef het zoekveld voorrang op functie toetsen hierna
+  $('.mp-SearchForm-query #input').on('keyup', function (e) {
+    if (e.keyCode == 13) {
+      $('button.mp-SearchForm-search').click()
     }
+  })
+
+  setUnsetListingThumbnails()
+}
+waitForKeyElements('.mp-Listing--list-item', alterSearchResults)
+
+function setUnsetListingThumbnails () {
+  $('.mp-Listing-image-item img').each(function () {
+    $(this).attr('src', $(this).data('src'))
+  })
 }
 
-function alterElsewhere() {
-    // Laat elders de checkbox "Zoek in titel en beschrijving" zien als optie.
-    $('header.u-stickyHeader').attr("data-expanded","true");
-    $("div.mp-Header.mp-text-paragraph.mp-cloak").addClass("mp-Header--expandSearchBar");
+function addMaps () {
+  // Maak locatie klikbaar, met OpenStreetMaps
+  const osmUrl = 'https://www.openstreetmap.org/search?query=' + $('#vip-seller-location span.name').text()
+  $('#vip-seller-location .heading span').replaceWith('<a target="_blank" href="' + osmUrl + '">' + $('#vip-seller-location span.name').text() + '</a>')
 }
-waitForKeyElements("header.u-stickyHeader div.mp-Header.mp-text-paragraph.mp-cloak", alterElsewhere);
+waitForKeyElements('#vip-seller-location .heading span', addMaps)
 
-function alterSearchResults() {
-    // Verberg betaalde advertenties en bedrijven
-    $('.mp-Listing--list-item').filter(":contains(Bezorgt in)").hide();
-    $('.mp-Listing--list-item').filter(":contains(Topadvertentie)").hide();
-    $('.mp-Listing--list-item').filter(":contains(Dagtopper)").hide();
-    $('.mp-Listing--list-item').filter(":contains(Heel Nederland)").hide();
-    $('.mp-Listing--list-item').filter(":contains(Bezoek website)").hide();
-
-    // Nutteloze "Deze adverteerder heeft meer advertenties" elementen verbergen
-    $(".mp-Listing--other-seller").hide();
-
-    // Adverteerders blokkeren
-    $('.mp-Listing--list-item').filter(":contains(Example Name 123)").hide();
-
-    // Geef het zoekveld voorrang op functie toetsen hierna
-    $('.mp-SearchForm-query #input').on('keyup', function (e) {
-        if (e.keyCode == 13) {
-            $("button.mp-SearchForm-search").click();
-        }
-    });
-
-    setUnsetListingThumbnails();
+function hideDetailCluter () {
+  // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
+  $('.cas-other-items').hide()
+  $('.banner-viptop').hide()
 }
-waitForKeyElements(".mp-Listing--list-item", alterSearchResults);
+waitForKeyElements('.cas-other-items', hideDetailCluter)
 
-function setUnsetListingThumbnails() {
-    $( ".mp-Listing-image-item img" ).each(function() {
-        $( this ).attr('src',$( this ).data('src'));
-    });
+function hideResultCluter () {
+  // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
+  $('.mp-Banner').hide()
+  $('.mp-Listings__admarktTitle').parent('div').hide()
+  $('#adsense-container').hide()
 }
+waitForKeyElements('.mp-Listings__admarktTitle', hideResultCluter)
 
-function addMaps() {
-    // Maak locatie klikbaar, met OpenStreetMaps
-    var osmUrl = "https://www.openstreetmap.org/search?query="+$("#vip-seller-location span.name").text();
-    $("#vip-seller-location .heading span").replaceWith('<a target="_blank" href="'+osmUrl+'">'+$("#vip-seller-location span.name").text()+'</a>');
+function buttonEvents () {
+  // Functies binden aan toetsen; volgende, vorige, zoekveld focus.
+  $('body').keyup(function (event) {
+    // Negeer wanneer er een veld in gebruik is
+    if ($(event.target).is('input, textarea')) {
+      return
+    }
+
+    // Naar de volgende pagina, met de rechter pijltoets
+    if (event.which == '39') {
+      $('.mp-PaginationControls-pagination a:nth-of-type(2)').click()
+      // Naar de vorige pagina, met de linker pijltoets
+    } else if (event.which == '37') {
+      $('.mp-PaginationControls-pagination a:nth-of-type(1)').click()
+      // Focus en selecteer direct het zoekveld door op '\' te drukken (want '/' is in gebruik door Firefox)
+    } else if (event.which == '220') {
+      $('.mp-SearchForm-query #input').select()
+    }
+  })
 }
-waitForKeyElements("#vip-seller-location .heading span", addMaps);
-
-function hideDetailCluter() {
-    // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
-    $(".cas-other-items").hide();
-    $(".banner-viptop").hide();
-}
-waitForKeyElements(".cas-other-items", hideDetailCluter);
-
-function hideResultCluter() {
-    // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
-    $(".mp-Banner").hide();
-    $(".mp-Listings__admarktTitle").parent('div').hide();
-    $("#adsense-container").hide();
-}
-waitForKeyElements(".mp-Listings__admarktTitle", hideResultCluter);
-
-
-function buttonEvents() {
-    // Functies binden aan toetsen; volgende, vorige, zoekveld focus.
-    $("body").keyup(function(event) {
-        // Negeer wanneer er een veld in gebruik is
-        if ($(event.target).is('input, textarea')) {
-            return;
-        }
-
-        // Naar de volgende pagina, met de rechter pijltoets
-        if (event.which == '39') {
-            $(".mp-PaginationControls-pagination a:nth-of-type(2)").click();
-            // Naar de vorige pagina, met de linker pijltoets
-        } else if (event.which == '37') {
-            $(".mp-PaginationControls-pagination a:nth-of-type(1)").click();
-            // Focus en selecteer direct het zoekveld door op '\' te drukken (want '/' is in gebruik door Firefox)
-        } else if (event.which == '220' ) {
-            $('.mp-SearchForm-query #input').select();
-        }
-    });
-}
-waitForKeyElements(".mp-PaginationControls-pagination a:nth-of-type(2)", buttonEvents);
+waitForKeyElements('.mp-PaginationControls-pagination a:nth-of-type(2)', buttonEvents)
