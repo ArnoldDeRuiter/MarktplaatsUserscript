a4ac84282b15c2cf076dc9c39acddfeb06f9a270
Arnold de Ruiter <187004+ArnoldDeRuiter@users.noreply.github.com>
2022-12-31 13:09:52 +0100

    Patched breaking changes on Marktplaats

--- JustTheSearchResults.user.js
+++ JustTheSearchResults.user.js
@@ -1,22 +1,20 @@
 // ==UserScript==
 // @name           Marktplaats; enkel de zoekresultaten.
-// @version        1.4.3
+// @version        1.5.0
 // @namespace      Marktplaats
 // @description    * Verbergt bedrijf/webshop advertenties
 // @description    * Verbergt betaalde advertenties
 // @description    * Schoont pagina's op door verbergen van nutteloze onderdelen
-// @description    * Voegt een link toe naar de locatie van de adverteerder
+// @description    * Voegt een OSM link toe naar de locatie van de adverteerder
 // @description    * Navigeer gemakkelijker doormiddel van sneltoetsen
-// @description    * eBay style zoekveld focus op Home
 // @description    * In-code mogelijkheid om gemakkelijk een blacklist samen te stellen
 // @description    * Toont volledige gebruikersnamen in overzicht van biedingen
 // @include        http://www.marktplaats.nl/z.html?*
 // @include        http://www.marktplaats.nl/z/*.html?*
 // @include        http://www.marktplaats.nl/*
 // @include        https://www.marktplaats.nl/*
-// @copyright      2021 Arnold de Ruiter
 // @license        MIT License
-// @require        https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @require        https://code.jquery.com/jquery-3.6.2.slim.min.js
 // @require        https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // @grant          GM_addStyle
 // ==/UserScript==
@@ -24,128 +22,77 @@
 /* - The @grant directive is needed for GreaseMonkey users.
     A work around for a design change introduced in GM 1.0. It restores the sandbox.
 */
-
-function alterHome () {
-  // Toon altijd de zoek filters, niet pas wanneer je op het zoekveld zelf klikt
-  $('.mp-SearchFieldset-advanced').css('max-height', 'unset')
-}
-waitForKeyElements('.u-stickyHeader', alterHome)
-
-function preventLoginNudge () {
-  $('#login-nudge-root').remove()
-}
-waitForKeyElements('#login-nudge-root', preventLoginNudge)
-
-function alterSearchbar () {
-  // Laat de checkbox "Zoek in titel en beschrijving" zien als optie.
-  $('header.u-stickyHeader').attr('data-expanded', 'true')
-  $('div.mp-Header.mp-text-paragraph.mp-cloak').addClass('mp-Header--expandSearchBar')
-}
-waitForKeyElements('header.u-stickyHeader div.mp-Header.mp-text-paragraph.mp-cloak', alterSearchbar)
-
-function alterSellerOverviewSearch () {
-  // When searching on a seller's overview page (Meer advertenties), automatically select 'Van deze verkoper' when using the searchbar
-  $('input[name*=\'searchOnPage\']').prop('checked', true)
-}
-waitForKeyElements('input[name*=\'searchOnPage\']', alterSellerOverviewSearch, true)
-
-function alterSearchResults () {
+function alterSearchResults() {
   // Verberg betaalde advertenties en bedrijven
-  $('.mp-Listing--list-item').filter(':contains(Bezorgt in)').hide()
-  $('.mp-Listing--list-item').filter(':contains(Topadvertentie)').hide()
-  $('.mp-Listing--list-item').filter(':contains(Dagtopper)').hide()
-  $('.mp-Listing--list-item').filter(':contains(Heel Nederland)').hide()
-  $('.mp-Listing--list-item').filter(':contains(Bezoek website)').hide()
+  $('[class*=Listing--list-item]').filter(':contains(Bezorgt in)').hide()
+  $('[class*=Listing--list-item]').filter(':contains(Topadvertentie)').hide()
+  $('[class*=Listing--list-item]').filter(':contains(Dagtopper)').hide()
+  $('[class*=Listing--list-item]').filter(':contains(Heel Nederland)').hide()
+  $('[class*=Listing--list-item]').filter(':contains(Bezoek website)').hide()
 
-  // In plaats van gewoon "Nieuwe voorraad" te controleren en verbergen, verberg de verkoper hiermee op de huidige pagina
-  const badSellersWithStock = $('.mp-Listing-Opvalsticker-wrapper').parents('.mp-Listing--list-item').find('.mp-Listing-seller-name')
+  // In plaats van "Nieuwe voorraad" te controleren en verbergen, verberg de verkoper hiermee op de huidige pagina
+  const badSellersWithStock = $('[class*=Listing-Opvalsticker-wrapper]').parents('[class*=Listing--list-item]').find('[class*=Listing-seller-name]')
   badSellersWithStock.each(function (i, el) {
-    $('.mp-Listing--list-item').filter(':contains(' + el.textContent + ')').hide()
+    $('[class*=Listing--list-item]').filter(':contains(' + el.textContent + ')').hide()
   })
 
-  // In plaats van gewoon een advertentie met sub afbeeldingen te controleren en verbergen, verberg de verkoper hiermee op de huidige pagina
-  const badSellersWithSubImages = $('.mp-Listing-sub-images').parents('.mp-Listing--list-item').find('.mp-Listing-seller-name')
+  // In plaats van een advertentie met sub afbeeldingen te controleren en verbergen, verberg de verkoper hiermee op de huidige pagina
+  const badSellersWithSubImages = $('[class*=Listing-sub-images]').parents('[class*=Listing--list-item]').find('[class*=Listing-seller-name]')
   badSellersWithSubImages.each(function (i, el) {
-    $('.mp-Listing--list-item').filter(':contains(' + el.textContent + ')').hide()
+    $('[class*=Listing--list-item]').filter(':contains(' + el.textContent + ')').hide()
   })
 
   // Nutteloze "Deze adverteerder heeft meer advertenties" elementen verbergen
-  $('.mp-Listing--other-seller').hide()
+  $('[class*=Listing--other-seller]').hide()
 
   // Adverteerders blokkeren
-  $('.mp-Listing--list-item').filter(':contains(Example Name 123)').hide()
-
-  // Clean up overview
-  $('.mp-Listing .mp-Attribute--micro-tip').parent().append($('.mp-Listing .mp-Attribute--micro-tip'))
-  $('.mp-Listing .mp-Attribute--micro-tip').addClass('mp-Attribute--default')
-  $('.mp-Listing .mp-Attribute--micro-tip').removeClass('mp-Attribute--micro-tip')
-  addGlobalStyle('.mp-Listing .mp-Attribute--micro-tip { padding: 0; background: none }')
-  addGlobalStyle('.mp-Listing--list-item .mp-Listing-group--price-date-feature { max-width: 25% }')
+  $('[class*=Listing--list-item]').filter(':contains(Example Name 123)').hide()
 
   setUnsetListingThumbnails()
 }
-waitForKeyElements('.mp-Listing--list-item', alterSearchResults, false)
+waitForKeyElements('[class*=Listing--list-item]', alterSearchResults, false)
 
-function setUnsetListingThumbnails () {
-  $('.mp-Listing-image-item img').each(function () {
-    $(this).attr('src', $(this).data('src'))
+function setUnsetListingThumbnails() {
+  $('[class*=Listing-image-item]').each(function () {
+    $(this).find('img').attr('src', $(this).find('img').data('src'))
   })
 }
 
-function improveSellerAdPage () {
+function improveSellerAdPage() {
   // Maak locatie klikbaar, met OpenStreetMaps
-  const osmUrl = 'https://www.openstreetmap.org/search?query=' + $('#vip-seller-location span.name').text()
-  $('#vip-seller-location .heading span').replaceWith('<a target="_blank" href="' + osmUrl + '">' + $('#vip-seller-location span.name').text() + '</a>')
-
-  // Vervang nutteloze 'Meer advertenties' scroll knop, met de daadwerkelijke directe link naar de advertenties va
-  const sellerProfile = $('.top-info a').attr('href')
-  $('#vip-seller-all-ads').append('<a href="' + sellerProfile + '">Bekijk alle advertenties</a>')
-  $('.do_scroll').remove()
+  const locationSpan = $('[class*=SvgIconLocation]').parent().find('span')
+  const osmUrl = 'https://www.openstreetmap.org/search?query=' + locationSpan.text()
+  locationSpan.replaceWith('<a target="_blank" href="' + osmUrl + '">' + locationSpan.text() + '</a>')
 }
-waitForKeyElements('#vip-seller-location .heading span', improveSellerAdPage)
+waitForKeyElements('[class*=SvgIconLocation]', improveSellerAdPage)
 
-function hideDetailCluter () {
-  // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
-  $('.cas-other-items').hide()
-  $('.banner-viptop').hide()
-}
-waitForKeyElements('.cas-other-items', hideDetailCluter)
-
-function hideResultCluter () {
-  // Opschonen van pagina, waar een ad blocker ruimtes achterlaat.
-  $('.mp-Banner').hide()
-  $('.mp-Listings__admarktTitle').parent('div').hide()
-  $('#adsense-container').hide()
-}
-waitForKeyElements('.mp-Listings__admarktTitle', hideResultCluter)
-
-function buttonEvents () {
+function buttonEvents() {
   // Functies binden aan toetsen; volgende, vorige, zoekveld focus.
   $('body').keyup(function (event) {
     // Naar de volgende pagina, met de rechter pijltoets
     if (event.which === 39) {
-      $('.mp-PaginationControls-pagination .mp-Button').last()[0].click()
+      $('[class*=PaginationControls-pagination]').find('[class*=Button]').last()[0].click()
       // Naar de vorige pagina, met de linker pijltoets
     } else if (event.which === 37) {
-      $('.mp-PaginationControls-pagination .mp-Button').first()[0].click()
+      $('[class*=PaginationControls-pagination]').find('[class*=Button]').first()[0].click()
     }
   })
 }
-waitForKeyElements('.mp-PaginationControls-pagination .mp-Button', buttonEvents)
+waitForKeyElements('[class*=PaginationControls-pagination]', buttonEvents)
 
-function hideAds () {
-  $('.mp-Listings--gallery-view').parent('.mp-Listings-bottomBlockGallery').hide()
-}
-waitForKeyElements('.mp-Listings--gallery-view', hideAds)
-
-function addGlobalStyle (css) {
-  let head, style
-  head = document.getElementsByTagName('head')[0]
-  if (!head) { return }
-  style = document.createElement('style')
+function addGlobalStyle(css) {
+  const head = document.getElementsByTagName('head')[0]
+  if (!head) {
+    return
+  }
+  const style = document.createElement('style')
   style.type = 'text/css'
   style.innerHTML = css
   head.appendChild(style)
 }
 addGlobalStyle('.ellipsis { white-space: normal; }')
-addGlobalStyle('.mp-Banner.mp-Banner--fluid { display: none }')
+addGlobalStyle('[class*="Banner--fluid"]{ display: none }')
+addGlobalStyle('[class*="Listings__container--cas"]{ display: none }')
+addGlobalStyle('#adsense-container{ display: none }')
+addGlobalStyle('#sustainability-visibility-tracker{ display: none }')
+addGlobalStyle('#login-nudge-root{ display: none }')
--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 Marktplaats Userscript
 
-Marktplaats heeft het opgegeven moment wat lastig gemaakt om werkelijke advertenties te vinden, tussen alle bedrijven, webshops en betaalde advertenties. :thumbsdown:
+Marktplaats heeft het opgegeven moment wat lastig gemaakt om de werkelijke advertenties te vinden, tussen alle bedrijven, webshops en betaalde advertenties. :thumbsdown:
 
 Dit Userscript maakt hier een einde aan en zorgt voor een overzichterlijke, gemakkelijke en betere gebruikers ervaring. :v:
 
-Functionaliteit op het moment:
+Functionaliteiten op dit moment:
 
 
 * Verbergt bedrijf/webshop advertenties
 * Verbergt betaalde advertenties
 * Schoont pagina's op door verbergen van nutteloze onderdelen
-* Voegt een link toe naar de locatie van de adverteerder
+* Voegt een OSM link toe naar de locatie van de adverteerder
 * Navigeer gemakkelijker doormiddel van sneltoetsen
 * In-code mogelijkheid om gemakkelijk een blacklist samen te stellen
-* Indien op een overzicht pagina van een verkoper wordt er standaard gezocht binnen dat overzicht
+* Toont volledige gebruikersnamen in overzicht van biedingen
 
 Suggesties, ideeën en PR's zijn natuurlijk altijd welkom! :octocat:
